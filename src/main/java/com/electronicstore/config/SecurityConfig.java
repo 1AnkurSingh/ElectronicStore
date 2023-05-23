b0a997f783936347edbe0ff5195abb31c9022593
Ankur Singh <ankurrana231002@gmail.com>
2023-05-23 10:19:46 +0530

    form based login customization

--- src/main/java/com/electronicstore/config/SecurityConfig.java
+++ src/main/java/com/electronicstore/config/SecurityConfig.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -35,6 +37,25 @@ public class SecurityConfig {
 //        InMemoryUserDetailsManager  is implementation class of UserDetailService
 //        return new InMemoryUserDetailsManager(normalUser,adminUser);
 //    }
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                        .anyRequest()
+//                                .authenticated()
+//                                        .and().
+//                formLogin()
+//                .loginPage("login.html")
+//                .loginProcessingUrl("/process-url")
+//                .defaultSuccessUrl("/dashboard")
+//                .failureUrl("error");
+//
+//
+//        return http.build();
+//    }
+
+
+
     @Bean
         DaoAuthenticationProvider daoAuthenticationProvider(){
         DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
